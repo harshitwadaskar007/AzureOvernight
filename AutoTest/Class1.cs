134a6b5852375af5b32d45874ea84829c2bc662a
harshitwadaskar007 <50833271+harshitwadaskar007@users.noreply.github.com>
2019-05-27 00:06:29 +0530

    Add project files.

---
+++ AutoTest/Class1.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OvernightExecution
+{
+    [TestFixture]
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+            IWebDriver driver = new ChromeDriver(path, chromeOptions);
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.Close();
+        }
+
+        [Test]
+        public void SecondTest()
+        {
+
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+            IWebDriver driver = new ChromeDriver(path, chromeOptions);
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.Close();
+        }
+    }
+}
